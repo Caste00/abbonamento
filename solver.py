ab0912cde02671c6898ec8db59d65697c811d667
Davide <155450298+Caste00@users.noreply.github.com>
2026-06-03 09:30:09 +0000

    m

--- solver.py
+++ solver.py
@@ -34,3 +34,27 @@ def solver(n, travel_days, costs, durations):
         choise[i] = ("buy", best_type, best_next)
 
     return dp, choise
+
+
+def reconstruct(choice, durations, n):
+    result = []
+
+    i = 0
+
+    while i < n:
+        action = choice[i]
+
+        if action[0] == "skip":
+            i += 1
+            continue
+
+        _, abbonamento, _ = action
+
+        start = i
+        end = min(n - 1, i + durations[abbonamento] - 1)
+
+        result.append((start, end, abbonamento))
+
+        i += durations[abbonamento]
+
+    return result
